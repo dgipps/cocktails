f6700e3d37f382e88b2a50af9324bcd9d03afd4b
Daniel Gipps <dgipps@gmail.com>
2019-07-27 10:35:58 -0400

    Get recipe view working

--- ingredients/views.py
+++ ingredients/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
 
 from ingredients.models import Ingredient, IngredientCategory, Recipe
 from ingredients.serializers import RecipeSerializer
@@ -12,13 +14,16 @@ class RecipeViewSet(viewsets.ModelViewSet):
         user = request.user
 
         available_categories = IngredientCategory.objects.filter(
-            ingredients__inventories__user_id=user.id
+            ingredients__inventories__user_id=user.id,
         )
 
         available_ingredients = Ingredient.objects.filter(
-
-        )
+            inventories__user_id=user.id,
+        ).values_list('id')
+        print(available_ingredients)
         recipes = Recipe.objects.filter(
-
+            ingredients__ingredient_id__in=available_ingredients,
         )
+        print(recipes)
         serializer = RecipeSerializer(recipes, many=True)
+        return Response(serializer.data)
